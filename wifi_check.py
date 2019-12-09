ea104939dcb0b2c155bf03aea7e978e2d05d8523
Timothy Ellsworth Bowers <tbowers7@gmail.com>
2019-12-09 16:37:29 -0700

    Update wifi_check.py
    Add second method for checking internet and/or WiFi connectivity

--- wifi_check.py
+++ wifi_check.py
@@ -1,4 +1,5 @@
 import urllib2
+import socket
 
 def wifi_on():
   try:
@@ -6,3 +7,21 @@ def wifi_on():
     return True
   except urllib2.URLError as err:
     return False
+
+  
+def wifi_on2(host='8.8.8.8', port=53, timeout=3):
+  """
+  Host: 8.8.8.8 (google-public-dns-a.google.com)
+  OpenPort: 53/tcp
+  Service: domain (DNS/TCP)
+  """
+  try:
+     socket.setdefaulttimeout(timeout)
+     socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+     return True
+   except socekt.error as ex:
+     print(ex)
+     return False
+
+wifi_on()
+wifi_on2()
