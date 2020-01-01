1ed1c1aa8714762099d33b1407e5d4572e0c3589
Chicken Pi <chickenpi@gmail.com>
2020-01-01 09:48:00 -0700

    Add display routine for DTH22 sensors
    Add routine to display the values from the DHT22 sensors in a Tk window.
    This routine polls the DHT sensors every 5 seconds, with error checking
    to ensure the data collected makes sense.
    
    	new file:   display_coop_values.py
    	modified:   testing/test_dht22.py

---
+++ display_coop_values.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+  FILE: display_coop_values.py
+
+Displays the values from chicken-pi sensors to the screen for visual inspection.
+
+"""
+
+# Futures
+# […]
+
+# Built-in/Generic Imports
+# […]
+
+# Libs
+from tkinter import *
+import time
+import datetime
+import adafruit_dht
+import board
+# […]
+
+# Own modules
+#from {path} import {class}
+# […]
+
+
+## Boilerplate variables
+__author__ = 'Timothy P. Ellsworth Bowers'
+__copyright__ = 'Copyright 2019-2020, chicken-pi'
+__credits__ = ['Stephen Bowers']
+__license__ = 'LGPL-3.0'
+__version__ = '0.1.0'
+__email__ = 'chickenpi@gmail.com'
+__status__ = 'Development Status :: 1 - Planning'
+
+
+"""
+  Layout of this file:
+  1.  Define the DISPLAY STRING
+  2.  Method for updating DISPLAY STRING
+  3.  Call the loop for Tk to DISPLAY STRING
+"""
+
+# (1) Define the DISPLAY STRING
+root = Tk()
+val1 = ''
+disp = Label(root, font=('courier', 25, 'bold'), bg='black', fg='yellow')
+disp.pack(fill=BOTH, expand=1)
+
+dht1 = adafruit_dht.DHT22(board.D19)
+dht2 = adafruit_dht.DHT22(board.D20)
+dht3 = adafruit_dht.DHT22(board.D21)
+
+
+
+
+# (2) Method for updating DISPLAY STRING
+def update():
+    global val1   # Make val1 available globally
+
+    ### Read from the DHT sensors
+    # Sensor 1
+    goodRead = False
+    while goodRead == False:
+        try:
+            tmp1 = dht1.temperature
+            hmd1 = dht1.humidity
+            if hmd1 < 100:         # Quality control
+                goodRead = True
+            else:
+                time.sleep(0.1)    # Wait a moment, and try again
+        except:
+            time.sleep(0.1)  # If error, wait a moment, and try again
+
+    # Sensor 2
+    goodRead = False
+    while goodRead == False:
+        try:
+            tmp2 = dht2.temperature
+            hmd2 = dht2.humidity
+            if hmd2 < 100:         # Quality control
+                goodRead = True
+            else:
+                time.sleep(0.1)    # Wait a moment, and try again
+        except:
+            time.sleep(0.1)  # If error, wait a moment, and try again
+
+    # Sensor 3
+    goodRead = False
+    while goodRead == False:
+        try:
+            tmp3 = dht3.temperature
+            hmd3 = dht3.humidity
+            if hmd3 < 100:         # Quality control
+                goodRead = True
+            else:
+                time.sleep(0.1)    # Wait a moment, and try again
+        except:
+            time.sleep(0.1)  # If error, wait a moment, and try again
+
+    ### Once we have good readings from all three sensors, do the C -> F
+    ### conversion and then create the output string
+    tmpf1 = tmp1 * (9. / 5.) + 32.
+    tmpf2 = tmp2 * (9. / 5.) + 32.
+    tmpf3 = tmp3 * (9. / 5.) + 32.
+
+    # String includes:
+    #   DATE
+    #   DHT1: Temp, Humidity
+    #   DHT2: Temp, Humidity
+    #   DHT3: Temp, Humidity
+    
+    now = datetime.datetime.now()
+    val2 = "{0:s}\n\n DHT #1: {1:0.1f}\xb0F, {2:0.1f}% \n DHT #2: {3:0.1f}\xb0F, {4:0.1f}% \n DHT #3: {5:0.1f}\xb0F, {6:0.1f}% ".format(
+        now.strftime("%d-%b-%y %H:%M:%S"),tmpf1, hmd1, tmpf2, hmd2, tmpf3, hmd3)
+
+    # Update the DISPLAY STRING
+    if val2 != val1:
+        val1 = val2
+        disp.config(text=val2)
+
+    # This Method calls itself every 5s to update the display
+    disp.after(5000, update)
+        
+
+
+# (3) Call the loop for Tk to DISPLAY STRING
+update()
+root.winfo_toplevel().title("Chicken Coop Sensor Values")
+root.mainloop()
--- testing/test_dht22.py
+++ testing/test_dht22.py
@@ -1,19 +1,91 @@
 import time
+from tkinter import *
 
 import adafruit_dht
-from board import D19
+import board
 
-dht = adafruit_dht.DHT22(D19)
 
-while True:
-    try:
-        temperature = dht.temperature
-        degf = temperature * (9 / 5) + 32
-        humidity = dht.humidity
-        # Print what we got to the REPL
-        print("Temp: {:.1f} *F \t Humidity: {}%".format(degf, humidity))
-    except RuntimeError as e:
-        # Reading doesn't always work! Just print error and we'll try again
-        print("Reading from DHT failure: ", e.args)
+class App:
 
-    time.sleep(1)
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+        self.read()
+        Label(frame, text='Values 1').grid(row=0, column=0)
+        Label(frame, text='Values 2').grid(row=1, column=0)
+        Label(frame, text='Values 3').grid(row=2, column=0)
+        
+        # Create the sliders and position them in a grid layout
+        # the 'command' attribute specifys a method to call when
+        # a slider is moved
+        scaleRed = Scale(frame, from_=0, to=100,
+              orient=HORIZONTAL, command=self.updateRed)
+        scaleRed.grid(row=0, column=1)
+        scaleGreen = Scale(frame, from_=0, to=100,
+              orient=HORIZONTAL, command=self.updateGreen)
+        scaleGreen.grid(row=1, column=1)
+        scaleBlue = Scale(frame, from_=0, to=100,
+              orient=HORIZONTAL, command=self.updateBlue)
+        scaleBlue.grid(row=2, column=1)
+
+    # These methods called whenever a slider moves
+    def updateRed(self, duty):
+        print('RED!')
+    def updateGreen(self, duty):
+        print('GREEN!')
+    def updateBlue(self, duty):
+        print('BLUE!')
+ 
+
+
+    def read(self):
+        try:
+            temp1 = dht1.temperature
+            degf1 = temp1 * (9 / 5) + 32
+            humid1 = dht1.humidity
+            # Print what we got to the REPL
+            print("DHT22 #1: Temp: {:.1f} *F \t Humidity: {}%".format(degf1, humid1))
+        except RuntimeError as e:
+            # Reading doesn't always work! Just print error and we'll try again
+            print("Reading from DHT failure: ", e.args)
+
+
+        time.sleep(0.1)
+    
+        try:
+            temp2 = dht2.temperature
+            degf2 = temp2 * (9 / 5) + 32
+            humid2 = dht2.humidity
+            # Print what we got to the REPL
+            print("DHT22 #2: Temp: {:.1f} *F \t Humidity: {}%".format(degf2, humid2))
+        except RuntimeError as e:
+            # Reading doesn't always work! Just print error and we'll try again
+            print("Reading from DHT failure: ", e.args)
+
+        time.sleep(0.1)
+        
+        try:
+            temp3 = dht3.temperature
+            degf3 = temp3 * (9 / 5) + 32
+            humid3 = dht3.humidity
+            # Print what we got to the REPL
+            print("DHT22 #3: Temp: {:.1f} *F \t Humidity: {}%".format(degf3, humid3))
+        except RuntimeError as e:
+            # Reading doesn't always work! Just print error and we'll try again
+            print("Reading from DHT failure: ", e.args)
+
+
+# Main Program
+
+dht1 = adafruit_dht.DHT22(board.D19)
+dht2 = adafruit_dht.DHT22(board.D20)
+dht3 = adafruit_dht.DHT22(board.D21)
+
+
+
+
+# Setup the display window
+root = Tk()
+root.wm_title('Coop Temperature')
+app = App(root)
+root.mainloop()
