0351860bef3d1a118c8bdbd025d43cd3094556a7
Chicken Pi <chickenpi@gmail.com>
2020-01-05 17:13:02 -0700

    Add read from Pi's CPU thermister
    Add reading from the Raspberry Pi's CPU thermister to monitor the CPU temp
    during operation.  CPU maximum temperature is 85C.
    
    	modified:   display_coop_values.py

--- display_coop_values.py
+++ display_coop_values.py
@@ -66,7 +66,7 @@ abspath = os.path.abspath(os.path.dirname(sys.argv[0]))
 fn = abspath+'/temp_values.csv'
 if not os.path.isfile(fn):
     csvfile = open(fn, 'w', newline='')
-    csvfile.write("date,time,t1,h1,t2,h2,t3,h3\n")
+    csvfile.write("date,time,t1,h1,t2,h2,t3,h3,cpu\n")
 else:
     csvfile = open(fn, 'a', newline='')
 atexit.register(csvfile.close)
@@ -98,11 +98,17 @@ def update():
             except:
                 time.sleep(0.1)  # If error, wait a moment, and try again
 
-
+    ### Also read from the on-board temperature sensors on the Pi
+    cputemp_fn = "/sys/class/thermal/thermal_zone0/temp"
+    f = open(cputemp_fn,"r")
+    if f.mode == 'r':
+        cpuTemp = float(f.read())/1000.
+    f.close()
+    
     ### Once we have good readings from all three sensors, do the C -> F
     ### conversion and then create the output string (numpy is our friend)
     tf = tc * (9. / 5.) + 32.
-
+    
     # String includes:
     #   DATE
     #   DHT1: Temp, Humidity
@@ -110,19 +116,19 @@ def update():
     #   DHT3: Temp, Humidity
     
     now = datetime.datetime.now()
-    val2 = "{:s}\n\n DHT #1: {:0.1f}\xb0F, {:0.1f}% \n DHT #2: {:0.1f}\xb0F, {:0.1f}% \n DHT #3: {:0.1f}\xb0F, {:0.1f}% ".format(
+    val2 = "{:s}\n\n DHT #1: {:0.1f}\xb0F, {:0.1f}% \n DHT #2: {:0.1f}\xb0F, {:0.1f}% \n DHT #3: {:0.1f}\xb0F, {:0.1f}% \n CPU: {:0.1f}\xb0C [< 85\xb0C] ".format(
         now.strftime("%d-%b-%y %H:%M:%S"),
-        tf[0], hm[0], tf[1], hm[1], tf[2], hm[2])
+        tf[0], hm[0], tf[1], hm[1], tf[2], hm[2], cpuTemp)
 
     # Update the DISPLAY STRING
     if val2 != val1:
         val1 = val2
         disp.config(text=val2)
-
+    
     # Write a line to the CSV file
     datawriter.writerow([now.strftime("%Y-%m-%d"),now.strftime("%H:%M:%S"),
-                         "{:0.2f},{:0.1f},{:0.2f},{:0.1f},{:0.2f},{:0.1f}".format(tf[0], hm[0], tf[1], hm[1], tf[2], hm[2])])
-
+                         "{:0.2f},{:0.1f},{:0.2f},{:0.1f},{:0.2f},{:0.1f},{:0.1f}".format(tf[0], hm[0], tf[1], hm[1], tf[2], hm[2], cpuTemp)])
+    
     # This Method calls itself every 5s to update the display
     disp.after(5000, update)
         
