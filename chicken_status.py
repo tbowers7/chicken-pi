07fb1c5a19ea85ae0f37d209f5d8b137b62cf805
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2021-07-27 11:55:53 -0700

    Reorder status window
    	modified:   chicken_status.py

--- chicken_status.py
+++ chicken_status.py
@@ -47,6 +47,9 @@ WIDGET_HIGH  = 300           # Height of the "Status Window"
 STATBG       = 'black'
 FONTSIZE     = 13
 DATAFIELD    = 15
+ENVROW       = 1
+DEVROW       = 5
+NETROW       = 9
 
 if os.path.exists("/usr/bin/uname"):
     uname = "/usr/bin/uname"
@@ -86,49 +89,49 @@ class StatusWindow():
         self.dispTime.grid(row=0, columnspan=4)
         
         # Create the various section labels
-        self.envLabel = self.make_sect_label('Environmental Status', row=1)
-        self.netLabel = self.make_sect_label('Network Status', row=5)
-        self.devLabel = self.make_sect_label('Device Status', row=8)
+        self.envLabel = self.make_sect_label('Environmental Status', row=ENVROW)
+        self.netLabel = self.make_sect_label('Network Status', row=NETROW)
+        self.devLabel = self.make_sect_label('Device Status', row=DEVROW)
 
         # Environmental Status Labels
-        self.envOutsLab = self.make_stat_label('    Outside:', row=2, column=0)
-        self.envNestLab = self.make_stat_label('      Nests:', row=3, column=0)
-        self.envRoosLab = self.make_stat_label('     Roosts:', row=4, column=0)
-        self.envLighLab = self.make_stat_label('      Light:', row=2, column=2)
-        self.envCPUTLab = self.make_stat_label('CPU (< 185):', row=3, column=2)
+        self.envOutsLab = self.make_stat_label('    Outside:', row=ENVROW+1, column=0)
+        self.envNestLab = self.make_stat_label('      Nests:', row=ENVROW+2, column=0)
+        self.envRoosLab = self.make_stat_label('     Roosts:', row=ENVROW+3, column=0)
+        self.envLighLab = self.make_stat_label('      Light:', row=ENVROW+1, column=2)
+        self.envCPUTLab = self.make_stat_label('CPU (< 185):', row=ENVROW+2, column=2)
 
         # Environmental Status Data
-        self.envOutsDat = self.make_stat_data(row=2, column=1)
-        self.envNestDat = self.make_stat_data(row=3, column=1)
-        self.envRoosDat = self.make_stat_data(row=4, column=1)
-        self.envLighDat = self.make_stat_data(row=2, column=3)
-        self.envCPUTDat = self.make_stat_data(row=3, column=3)
+        self.envOutsDat = self.make_stat_data(row=ENVROW+1, column=1)
+        self.envNestDat = self.make_stat_data(row=ENVROW+2, column=1)
+        self.envRoosDat = self.make_stat_data(row=ENVROW+3, column=1)
+        self.envLighDat = self.make_stat_data(row=ENVROW+1, column=3)
+        self.envCPUTDat = self.make_stat_data(row=ENVROW+2, column=3)
 
         # Network Status Labels
-        self.netWiFiLab = self.make_stat_label('WiFi Status:', row=6, column=0)
-        self.netInetLab = self.make_stat_label('WLAN Status:', row=7, column=0)
-        self.netLANaLab = self.make_stat_label('   Local IP:', row=6, column=2)
-        self.netWANaLab = self.make_stat_label('    WLAN IP:', row=7, column=2)
+        self.netWiFiLab = self.make_stat_label('WiFi Status:', row=NETROW+1, column=0)
+        self.netInetLab = self.make_stat_label('WLAN Status:', row=NETROW+2, column=0)
+        self.netLANaLab = self.make_stat_label('   Local IP:', row=NETROW+1, column=2)
+        self.netWANaLab = self.make_stat_label('    WLAN IP:', row=NETROW+2, column=2)
         
         # Network Status Data
-        self.netWiFiDat = self.make_stat_data(row=6, column=1)
-        self.netInetDat = self.make_stat_data(row=7, column=1)
-        self.netLANaDat = self.make_stat_data(row=6, column=3)
-        self.netWANaDat = self.make_stat_data(row=7, column=3)
+        self.netWiFiDat = self.make_stat_data(row=NETROW+1, column=1)
+        self.netInetDat = self.make_stat_data(row=NETROW+2, column=1)
+        self.netLANaDat = self.make_stat_data(row=NETROW+1, column=3)
+        self.netWANaDat = self.make_stat_data(row=NETROW+2, column=3)
 
         # Device Status Labels
-        self.devOut1Lab = self.make_stat_label('   Outlet 1:', row=9, column=0)
-        self.devOut2Lab = self.make_stat_label('   Outlet 2:', row=9, column=2)
-        self.devOut3Lab = self.make_stat_label('   Outlet 3:', row=10, column=0)
-        self.devOut4Lab = self.make_stat_label('   Outlet 4:', row=10, column=2)
-        self.devDoorLab = self.make_stat_label('       Door:', row=11, column=0)
+        self.devOut1Lab = self.make_stat_label('   Outlet 1:', row=DEVROW+1, column=0)
+        self.devOut2Lab = self.make_stat_label('   Outlet 2:', row=DEVROW+1, column=2)
+        self.devOut3Lab = self.make_stat_label('   Outlet 3:', row=DEVROW+2, column=0)
+        self.devOut4Lab = self.make_stat_label('   Outlet 4:', row=DEVROW+2, column=2)
+        self.devDoorLab = self.make_stat_label('       Door:', row=DEVROW+3, column=0)
 
         # Device Status Data
-        self.devOut1Dat = self.make_stat_data(row=9, column=1)
-        self.devOut2Dat = self.make_stat_data(row=9, column=3)
-        self.devOut3Dat = self.make_stat_data(row=10, column=1)
-        self.devOut4Dat = self.make_stat_data(row=10, column=3)
-        self.devDoorDat = self.make_stat_data(row=11, column=1)
+        self.devOut1Dat = self.make_stat_data(row=DEVROW+1, column=1)
+        self.devOut2Dat = self.make_stat_data(row=DEVROW+1, column=3)
+        self.devOut3Dat = self.make_stat_data(row=DEVROW+2, column=1)
+        self.devOut4Dat = self.make_stat_data(row=DEVROW+2, column=3)
+        self.devDoorDat = self.make_stat_data(row=DEVROW+3, column=1)
 
 
 
