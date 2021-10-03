3688d60ae6968f5963bbee95691e68eea4d5d86e
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2021-10-02 22:47:30 -0700

    More PEP8 cleaning
    	modified:   chicken_status.py

--- chicken_status.py
+++ chicken_status.py
@@ -8,13 +8,15 @@ Status display window, updates frequently with current values
 """
 
 # Built-In Libraries
-from tkinter import *
+from tkinter import Frame, Label
 import time
 import os
 
 # 3rd Party Libraries
 from requests import get
+from requests import exceptions as rexcept
 import urllib3
+from urllib3.exceptions import NewConnectionError, ConnectTimeoutError
 
 # Internal Imports
 try:
@@ -69,131 +71,179 @@ class StatusWindow():
         self.frame.pack(expand=0)
 
         # Put the time at the top of the window
-        self.dispTime = Label(self.frame, font=('courier', FONTSIZE, 'bold'),
+        self.display_time = Label(self.frame, font=('courier', FONTSIZE, 'bold'),
                               bg=STATBG, fg='skyblue')
-        self.dispTime.grid(row=0, columnspan=4)
+        self.display_time.grid(row=0, columnspan=4)
 
         # Create the various section labels
-        self.make_sect_label('Environmental Status', row=ENVROW)
-        self.make_sect_label('Network Status', row=NETROW)
-        self.make_sect_label('Device Status', row=DEVROW)
+        self.make_section_label('Environmental Status', row=ENVROW)
+        self.make_section_label('Network Status', row=NETROW)
+        self.make_section_label('Device Status', row=DEVROW)
 
         # Environmental Status Labels
-        self.make_stat_label('    Outside:', row=ENVROW+1, column=0)
-        self.make_stat_label('Inside Coop:', row=ENVROW+2, column=0)
-        self.make_stat_label('Light Level:', row=ENVROW+3, column=0)
-        self.make_stat_label(' Inside Box:', row=ENVROW+1, column=2)
-        self.make_stat_label('   RPi4 CPU:', row=ENVROW+2, column=2)
+        self.make_statistic_label('    Outside:', row=ENVROW+1, column=0)
+        self.make_statistic_label('Inside Coop:', row=ENVROW+2, column=0)
+        self.make_statistic_label('Light Level:', row=ENVROW+3, column=0)
+        self.make_statistic_label(' Inside Box:', row=ENVROW+1, column=2)
+        self.make_statistic_label('   RPi4 CPU:', row=ENVROW+2, column=2)
 
         # Environmental Status Data
-        self.envOutsDat = self.make_stat_data(row=ENVROW+1, column=1)
-        self.envInsiDat = self.make_stat_data(row=ENVROW+2, column=1)
-        self.envLighDat = self.make_stat_data(row=ENVROW+3, column=1)
-        self.envChPiDat = self.make_stat_data(row=ENVROW+1, column=3)
-        self.envCPUTDat = self.make_stat_data(row=ENVROW+2, column=3)
+        self.env_outside_data = self.make_statistic_data(row=ENVROW+1, column=1)
+        self.env_inside_data = self.make_statistic_data(row=ENVROW+2, column=1)
+        self.env_light_data = self.make_statistic_data(row=ENVROW+3, column=1)
+        self.env_pi_data = self.make_statistic_data(row=ENVROW+1, column=3)
+        self.env_cpu_data = self.make_statistic_data(row=ENVROW+2, column=3)
 
         # Network Status Labels
-        self.make_stat_label('WiFi Status:', row=NETROW+1, column=0)
-        self.make_stat_label('WLAN Status:', row=NETROW+2, column=0)
-        self.make_stat_label('   Local IP:', row=NETROW+1, column=2)
-        self.make_stat_label('    WLAN IP:', row=NETROW+2, column=2)
+        self.make_statistic_label('WiFi Status:', row=NETROW+1, column=0)
+        self.make_statistic_label('WLAN Status:', row=NETROW+2, column=0)
+        self.make_statistic_label('   Local IP:', row=NETROW+1, column=2)
+        self.make_statistic_label('    WLAN IP:', row=NETROW+2, column=2)
 
         # Network Status Data
-        self.netWiFiDat = self.make_stat_data(row=NETROW+1, column=1)
-        self.netInetDat = self.make_stat_data(row=NETROW+2, column=1)
-        self.netLANaDat = self.make_stat_data(row=NETROW+1, column=3)
-        self.netWANaDat = self.make_stat_data(row=NETROW+2, column=3)
+        self.net_wifi_data = self.make_statistic_data(row=NETROW+1, column=1)
+        self.new_internet_data = self.make_statistic_data(row=NETROW+2, column=1)
+        self.net_lanip_data = self.make_statistic_data(row=NETROW+1, column=3)
+        self.net_wanip_data = self.make_statistic_data(row=NETROW+2, column=3)
 
         # Device Status Labels
-        self.make_stat_label('   Outlet 1:', row=DEVROW+1, column=0)
-        self.make_stat_label('   Outlet 2:', row=DEVROW+2, column=0)
-        self.make_stat_label('   Outlet 3:', row=DEVROW+1, column=2)
-        self.make_stat_label('   Outlet 4:', row=DEVROW+2, column=2)
-        self.make_stat_label('       Door:', row=DEVROW+3, column=0)
+        self.make_statistic_label('   Outlet 1:', row=DEVROW+1, column=0)
+        self.make_statistic_label('   Outlet 2:', row=DEVROW+2, column=0)
+        self.make_statistic_label('   Outlet 3:', row=DEVROW+1, column=2)
+        self.make_statistic_label('   Outlet 4:', row=DEVROW+2, column=2)
+        self.make_statistic_label('       Door:', row=DEVROW+3, column=0)
 
         # Device Status Data
-        self.devOutDat = []
-        self.devOutDat.append(self.make_stat_data(row=DEVROW+1, column=1))
-        self.devOutDat.append(self.make_stat_data(row=DEVROW+2, column=1))
-        self.devOutDat.append(self.make_stat_data(row=DEVROW+1, column=3))
-        self.devOutDat.append(self.make_stat_data(row=DEVROW+2, column=3))
-        self.devDoorDat = self.make_stat_data(row=DEVROW+3, column=1)
+        self.dev_outlet_data = []
+        self.dev_outlet_data.append(self.make_statistic_data(row=DEVROW+1, column=1))
+        self.dev_outlet_data.append(self.make_statistic_data(row=DEVROW+2, column=1))
+        self.dev_outlet_data.append(self.make_statistic_data(row=DEVROW+1, column=3))
+        self.dev_outlet_data.append(self.make_statistic_data(row=DEVROW+2, column=3))
+        self.dev_door_data = self.make_statistic_data(row=DEVROW+3, column=1)
 
     # Label Creator Methods
-    def make_sect_label(self, text, row):
+    def make_section_label(self, text, row):
+        """make_section_label Make the Section labels
+
+        [extended_summary]
+
+        Parameters
+        ----------
+        text : `str`
+            The text for the section label
+        row : `int`
+            Which row of the GUI this label should be placed in
+
+        Returns
+        -------
+        `tkinter.Label`
+            The Label object
+        """
         label = Label(self.frame, font=('times', FONTSIZE, 'bold'),
                       bg=STATBG, fg='thistle2', text=text)
         label.grid(row=row, columnspan=4)
         return label
 
-    def make_stat_label(self, text, row, column):
+    def make_statistic_label(self, text, row, column):
+        """make_statistic_label Make a label for a given statistic
+
+        [extended_summary]
+
+        Parameters
+        ----------
+        text : `str`
+            The text for the statistic label
+        row : `int`
+            Which row of the GUI this label should be placed in
+        column : `int`
+            Which column of the GUI this label should be placed in
+
+        Returns
+        -------
+        `tkinter.Label`
+            The Label object
+        """
         label = Label(self.frame, font=('courier', FONTSIZE, 'bold'),
                       bg=STATBG, fg='lightgreen', text=text)
         label.grid(row=row, column=column)
         return label
 
-    def make_stat_data(self, row, column):
+    def make_statistic_data(self, row, column):
+        """make_statistic_data Make the data object for a given statistic
+
+        [extended_summary]
+
+        Parameters
+        ----------
+        row : `int`
+            Which row of the GUI this data should be placed in
+        column : `int`
+            Which column of the GUI this data should be placed in
+
+        Returns
+        -------
+        `tkinter.Label`
+            The Label object
+        """
         label = Label(self.frame, font=('courier', FONTSIZE, 'bold'),
                       bg=STATBG, fg='burlywood1', width=DATAFIELD)
         label.grid(row=row, column=column)
         return label
 
-    def format_th_str(self, temp, humid):
-        return f"{temp:.1f}\xb0F, {humid:.1f}%"
-
-    def format_lux_str(self, lux):
-        if lux is None:
-            return '-'*5
-        if lux < 10:
-            return f"{lux:.2f} lux"
-        if lux < 100:
-            return f"{lux:.1f} lux"
-        return f"{lux:.0f} lux"
-
-    def format_cpu_str(self, cputemp):
-        return f"{cputemp:0.0f}\xb0F (<185\xb0F)" if cputemp is not None \
-            else "----- (<185\xb0F)"
-
     def close_window(self):
+        """close_window Close the window
+        """
         self.master.destroy()
 
     def update(self, now, sensors, relays):
+        """update Update the information in this Window
 
+        [extended_summary]
+
+        Parameters
+        ----------
+        now : `datetime.datetime`
+            The current time at the update
+        sensors : `dict`
+            The dictionary containing the sensor objects
+        relays : `chicken_devices.Relay`
+            The Relay class
+        """
         # Get the current time, and write
-        self.dispTime.config(text =
+        self.display_time.config(text =
             now.strftime("%A, %B %-d, %Y    %-I:%M:%S %p"))
 
         # Set data update intervals
-        short_interval = True if now.second % 15 == 0 else False
-        long_interval = True if now.second % 60 == 0 else False
+        short_interval = now.second % 15 == 0
+        long_interval = now.second % 60 == 0
 
         # Write the various environment statuses at different intervals
         if short_interval:
-            self.envCPUTDat.config(text = self.format_cpu_str(get_cpu_temp()))
+            self.env_cpu_data.config(text = format_cpu_str(get_cpu_temp()))
         if long_interval:
-            self.envInsiDat.config(text =
-                self.format_th_str(sensors['inside'].temp, sensors['inside'].humid))
-            self.envOutsDat.config(text =
-                self.format_th_str(sensors['outside'].temp, sensors['outside'].humid))
-            self.envChPiDat.config(text =
-                self.format_th_str(sensors['box'].temp, sensors['box'].humid))
-            self.envLighDat.config(text =
-                self.format_lux_str(sensors['light'].level))
+            self.env_inside_data.config(text =
+                format_temp_humid_str(sensors['inside'].temp, sensors['inside'].humid))
+            self.env_outside_data.config(text =
+                format_temp_humid_str(sensors['outside'].temp, sensors['outside'].humid))
+            self.env_pi_data.config(text =
+                format_temp_humid_str(sensors['box'].temp, sensors['box'].humid))
+            self.env_light_data.config(text =
+                format_lux_str(sensors['light'].level))
 
         # Write the various device statuses at different intervals
         if short_interval:
-            for i, dev in enumerate(self.devOutDat):
+            for i, dev in enumerate(self.dev_outlet_data):
                 dev.config(text = 'ENERGIZED' if relays.state[i] else 'OFF')
 
         # Write the various network statuses
         if short_interval:
-            self.netLANaDat.config(text = get_local_IP())
-            self.netWiFiDat.config(text =
+            self.net_lanip_data.config(text = get_local_ipv4())
+            self.net_wifi_data.config(text =
                 'ON' if contact_server('192.168.0.1') else 'OFF')
         if long_interval:
-            self.netInetDat.config(text =
+            self.new_internet_data.config(text =
                 'ON' if contact_server('1.1.1.1') else 'OFF')
-            self.netWANaDat.config(text = get_public_IP())
+            self.net_wanip_data.config(text = get_public_ipv4())
 
         # For short update interval, wait an additional 0.5 s before returning
         if short_interval:
@@ -220,11 +270,11 @@ def contact_server(host='192.168.0.1'):
         http = urllib3.PoolManager()
         http.request('GET', host, timeout=3, retries=False)
         return True
-    except:
+    except (NewConnectionError, ConnectTimeoutError):
         return False
 
 
-def get_local_IP():
+def get_local_ipv4():
     """get_local_IP Return the local (LAN) IP address for the Pi
 
     Use `ifconfig` to read the local IP address assigned to the Pi by the
@@ -239,7 +289,7 @@ def get_local_IP():
     return (os.popen(cmd).read()).strip()
 
 
-def get_public_IP():
+def get_public_ipv4():
     """get_public_IP Return the public-facing IP address for the Pi
 
     Query ipify.org to return the public (WAN) IP address for the network
@@ -255,13 +305,13 @@ def get_public_IP():
         Public IP address
     """
     try:
-        publicIP = (get('https://api.ipify.org').text).strip()
+        public_ipv4 = (get('https://api.ipify.org', timeout=10).text).strip()
         # If response is longer than the maximum 15 characters, return '---'.
-        if len(publicIP) > 15:
-            publicIP = '-----'
-    except:
-        publicIP = '-----'
-    return publicIP
+        if len(public_ipv4) > 15:
+            public_ipv4 = '-----'
+    except (rexcept.ConnectionError, ConnectionError):
+        public_ipv4 = '-----'
+    return public_ipv4
 
 
 # Environmental Checking Functions
@@ -278,15 +328,82 @@ def get_cpu_temp():
     # Check Pi CPU Temp:
     if SYSTYPE == 'Linux':
         cputemp_fn = "/sys/class/thermal/thermal_zone0/temp"
-        with open(cputemp_fn,"r") as f:
-            return (float(f.read()) /1000.) * 9./5. + 32.
+        with open(cputemp_fn,"r") as sys_file:
+            return (float(sys_file.read()) /1000.) * 9./5. + 32.
     return None
 
 
 # Status Checking Functions
 def get_outlet_status():
+    """get_outlet_status Get the status of the outlets
+
+    [extended_summary]
+
+    Returns
+    -------
+    `list` of `bool`
+        A list of the True/False states for the 4 relays.
+    """
     try:
         # Read the relay_status
         return relay.status()
-    except:
+    except ValueError:
         return [False] * 4
+
+
+# String Formatting Functions
+def format_temp_humid_str(temp, humid):
+    """format_temp_humid_str Format the Temperature/Humidity strings
+
+    Parameters
+    ----------
+    temp : `float`
+        The temperature to be displayed (ºF)
+    humid : `float`
+        The humidity to be displayed (%)
+
+    Returns
+    -------
+    `str`
+        The properly formatted string
+    """
+    return f"{temp:.1f}\xb0F, {humid:.1f}%"
+
+
+def format_lux_str(lux):
+    """format_lux_str Formet the Lux strings
+
+    Parameters
+    ----------
+    lux : `float`
+        The light level to be displayed (lux)
+
+    Returns
+    -------
+    `str`
+        The properly formatted string
+    """
+    if lux is None:
+        return '-'*5
+    if lux < 10:
+        return f"{lux:.2f} lux"
+    if lux < 100:
+        return f"{lux:.1f} lux"
+    return f"{lux:.0f} lux"
+
+
+def format_cpu_str(cputemp):
+    """format_cpu_str Format the CPU temperature string
+
+    Parameters
+    ----------
+    cputemp : `float`
+        The CPU temperature to be displayed (ºC)
+
+    Returns
+    -------
+    `str`
+        The properly formatted string
+    """
+    return f"{cputemp:0.0f}\xb0F (<185\xb0F)" if cputemp is not None \
+        else "----- (<185\xb0F)"
